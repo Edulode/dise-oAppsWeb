027bcab704a3f7506ae2164f72adea2c8a49b405
Edulo <linopirno@gmail.com>
2025-04-01 19:56:05 -0600

    Login admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             GenderSeeder::class,
             UniverseSeeder::class,
             SuperheroeSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Universe;
-use App\Models\Gender;
-use App\Models\Superheroe;
-use App\Http\Controllers\GenderController;
-use App\Http\Controllers\UniverseController;
-use App\Http\Controllers\SuperheroeController;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('/genders', [GenderController::class, 'index']);
-//Route::get('/universes', [UniverseController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('genders', GenderController::class);
-Route::resource('universes', UniverseController::class);
-Route::resource('superheroes', SuperheroeController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
